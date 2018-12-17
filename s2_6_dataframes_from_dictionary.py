05d98beec10f67ca29c757231f0ab1225fdae1d8
mmchiro <mmchiro55@yahoo.com.sg>
2018-12-17 13:47:32 +0800

    Following lecture on Creating DataFrames from Dictionary

---
+++ s2_6_dataframes_from_dictionary.py
@@ -0,0 +1,23 @@
+import pandas as pd
+
+
+#course_sales = {'course':['Python','Ruby','Excel','C++'], 'day':['Mon','Tue','Wed','Tue'],'price':[5,10,15,20],'sale':[2,3,5,7]}
+
+#print(course_sales)
+#Converting dictionary into a dataframe. The keys of the dictionary become the dataframe columns.
+#df=pd.DataFrame(course_sales)
+#print(df)
+course = ['Python','Ruby','Excel','C++']
+day = ['Mon','Tue','Wed','Tue']
+price = [5,10,15,20]
+sales = [2,3,5,7]
+Labels = ['Course','Day','Price','# Sales']
+
+cols = [course,day,price,sales]
+
+master_list = list(zip(Labels,cols))
+print(master_list)
+
+data=dict(master_list)
+new_sales = pd.DataFrame(data)
+print(new_sales)
