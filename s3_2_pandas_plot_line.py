444d69b8e0fbd93a579a53a8e6d6cfca10fc1260
mmchiro <mmchiro55@yahoo.com.sg>
2018-12-17 19:37:10 +0800

    Following lecture on creating line plots

---
+++ s3_2_pandas_plot_line.py
@@ -0,0 +1,29 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+#Import data file
+stock_prices = pd.read_csv("intel_amd_stock_prices.csv")
+
+
+#Print data frame
+print(stock_prices)
+
+
+#Create y column
+y_columns=['intel','amd']
+
+
+# Assign axis
+stock_prices.plot(x='month',y=y_columns)
+
+
+#Create plot tilte
+plt.title('Monthly Stock Prices')
+
+
+#Create a title for y axis
+plt.ylabel('Prices (US$)')
+
+
+#Show the pyplot
+plt.show()
